0a396d96135ba80ed27947c99e5993dde043126e
Wunna8834 <wunnaaungsayargyi99@gmail.com>
2023-10-18 21:53:58 +0630

    Finished Backend

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,19 @@
 import express from 'express'
-
+import { createRoom, deleteRoom, getAllRooms, getRoom, updateRoom } from '../controllers/room.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 const router = express.Router()
 
+router.post("/:hotelid", verifyAdmin, createRoom)
 
+//update
+router.put("/:id", verifyAdmin, updateRoom);
+
+//delete 
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+//get
+router.get("/:id", getRoom);
+
+//get all
+router.get("/", getAllRooms);
 export default router
