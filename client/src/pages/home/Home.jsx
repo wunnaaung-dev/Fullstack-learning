3f24104dda62b031f365773fe3fc49bed7c6d17a
Wunna8834 <wunnaaungsayargyi99@gmail.com>
2023-10-19 11:18:08 +0630

    API request ya twr p

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Featured from '../../components/featured/Featured'
+
+export default function Home() {
+  return (
+    <div>
+        <p>Navbar</p>
+        <p>Header</p>
+        <Featured />
+        <p>Property List</p>
+        <p>Featured Property</p>
+        <p>Mail List</p>
+        <p>Footer</p>
+    </div>
+  )
+}
